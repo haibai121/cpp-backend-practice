779fa87c549e744b35887034d0a7973ebebf8045
cppdev <2410572553@qq.com>
2026-02-13 01:31:59 +0000

    text:add data race demonstration in week2

---
+++ week2/Data_Race/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <thread>
+
+volatile long counter = 0;
+
+void add_counter1(){
+    for(long i = 0; i < 100000000; i++){
+        counter++;
+    }
+    std::cout << "Done!\n";
+}
+
+void add_counter2(){
+    for(long j = 0; j < 100000000; j++){
+        counter++;
+    }
+    std::cout << "Done!\n";
+}
+
+int main(){
+    std::thread t1(add_counter1);
+    std::thread t2(add_counter2);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "All Done! counter = " << counter ;
+
+}
